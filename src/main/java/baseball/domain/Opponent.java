04ae22ccb035a711dd76cf1729c2d03b8623f32d
donghyunn1 <dongdong0516@naver.com>
2024-11-28 02:10:26 +0900

    feat: 자릿수를 비교하여 힌트를 제공할 메서드 기능 구현

--- src/main/java/baseball/domain/Opponent.java
+++ src/main/java/baseball/domain/Opponent.java
@@ -14,4 +14,21 @@ public class Opponent {
         List<Integer> answers = randomAnswerGenerator.generate();
         return new Opponent(answers);
     }
+
+    public Hint checkHint(List<Integer> userNumbers) {
+        Hint hint = new Hint();
+        for (int i = 0; i < userNumbers.size(); i++) {
+            int userDigit = userNumbers.get(i);
+            int opponentDigit = answers.get(i);
+
+            if (userDigit == opponentDigit) {
+                hint.increaseStrike();
+                continue;
+            }
+            if (answers.contains(userDigit)) {
+                hint.increaseBall();
+            }
+        }
+        return hint;
+    }
 }
